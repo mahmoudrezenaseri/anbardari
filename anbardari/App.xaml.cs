8d889991854c06dcd923cdb13990c33aefafa9ee
kaveh <mahmoudreza.naseri@gmail.com>
2023-07-06 01:29:04 +0330

    #2 Add wpf project with some simple windows and designs

---
+++ anbardari/App.xaml.cs
@@ -0,0 +1,73 @@
+﻿using anbardari.domain;
+using anbardari.domain.Repository.UserRepository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace anbardari
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static IHost? appHost { get; private set; }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            appHost = CreateHostBuilder(e.Args).Build();
+            await appHost!.StartAsync();
+
+            MigrateDbContext(appHost);
+
+            LoginWindow loginWindow = appHost.Services.GetRequiredService<LoginWindow>();
+            loginWindow.Show();
+
+            base.OnStartup(e);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            await appHost!.StopAsync();
+
+            base.OnExit(e);
+        }
+
+        #region - Methods -
+
+        private static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args)
+             .ConfigureAppConfiguration((hostBuilderContext, configurationBuilder) => { })
+             .ConfigureServices((hostContext, services) =>
+             {
+                 services.AddSingleton<LoginWindow>();
+                 services.AddScoped<IUserRepository, UserRepository>();
+                 services.AddDbContext<MyDbContext>(
+                     options =>
+                     {
+                         options.UseSqlServer("Server=.;Database=anbardaridb;User Id=sa;Password=Kaveh1!@;TrustServerCertificate=true;");
+                         options.UseLazyLoadingProxies();
+                     });
+             });
+
+        private static void MigrateDbContext(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            using (var dbContext = scope.ServiceProvider.GetRequiredService<MyDbContext>())
+            {
+                dbContext.Database.Migrate();
+
+                ILogger<MyDbContextSeed> logger = scope.ServiceProvider.GetRequiredService<ILogger<MyDbContextSeed>>();
+
+                MyDbContextSeed.SeedAsync(dbContext, logger).Wait();
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ anbardari/LoginWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using anbardari.domain;
+using anbardari.domain.Repository.UserRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace anbardari
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        private readonly IUserRepository _userRepository;
+      
+        public LoginWindow(IUserRepository userRepository)
+        {
+            InitializeComponent();
+
+            _userRepository = userRepository;            
+        }
+
+        private void Password_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void UserName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Rectangle_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private async void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            var users = await _userRepository.GetUsersAsync();
+            MessageBox.Show(users.Count().ToString());
+        }
+    }
+}
